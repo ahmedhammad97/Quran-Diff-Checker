b0d8a66768e4209c49b572bff86a4d2e244acd36
Ahmed Hammad <hammad97official@gmail.com>
2020-07-31 23:37:31 +0200

    Modify cleaner to allow more valid chars

--- processor/cleaner.js
+++ processor/cleaner.js
@@ -1,17 +1,50 @@
+var arabicNormChar = {
+  "ﻷ": "لا",
+  "ؤ": "و",
+  "ى": "ی",
+  "ي": "ی",
+  "ئ": "ی",
+  "أ": "ا",
+  "إ": "ا",
+  "آ": "ا",
+  "ٱ": "ا",
+  "ٳ": "ا",
+  "ء": "",
+  "ِ": "",
+  "ْ": "",
+  "ُ": "",
+  "َ": "",
+  "ّ": "",
+  "ٍ": "",
+  "ً": "",
+  "ٌ": "",
+  "ٓ": "",
+  "ٰ": "",
+  "ٔ": "",
+  "�": "",
+};
+
+var simplifyArabic = function (str) {
+  return str.replace(/[^\u0000-\u007E]/g, function (a) {
+    var retval = arabicNormChar[a];
+    if (retval == undefined) retval = a;
+    return retval;
+  }).normalize("NFKD").toLowerCase();
+};
+
 const removeTashkil = function (text) {
+  text = simplifyArabic(text);
   //remove special characters
+  text = text.normalize("NFKD").toLowerCase();
   text = text.replace(
-    /([^\u0621-\u063A\u0641-\u064A\u0660-\u0669a-zA-Z 0-9])/g,
+    /([\u0600-\u061F\u064B-\u066F\u0676-\u06C0\u06D6-\u08FF])/g,
     "",
   );
 
-  //normalize Arabic
-  text = text.replace(/(آ|إ|أ)/g, "ا");
-  text = text.replace(/(ة)/g, "ه");
-  text = text.replace(/(ئ|ؤ)/g, "ء");
-  text = text.replace(/(ى)/g, "ي");
+  text.replace(/([\u0622-\u0623\u0625\u0627\u0671-\u0673\u0675])/g, "ا");
+  text.replace(/([\u0620\u0626\u0649\u064A\u06CC-\u06CE\u06D0-\u06D3])/g, "ی");
 
-  return text;
+  return text.normalize("NFKD").toLowerCase();
 };
 
 module.exports = { removeTashkil };
